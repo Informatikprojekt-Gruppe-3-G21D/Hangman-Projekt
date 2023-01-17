59e3843fe96b31d657742bda0469c30f76e02ed1
michelle1111111 <122903393+michelle1111111@users.noreply.github.com>
2023-01-17 17:07:25 +0100

    Update Hangman finished.py

--- Hangman finished.py
+++ Hangman finished.py
@@ -102,7 +102,7 @@ if chancen == 0 and ("_ " in StricheFürBuchstaben) :
     print(" ¦                  ¦")
     print(" ¦                  ¦")
     print(" ¦                  O")
-    print("                  -----   Du bist gestorben!")
+    print("                  -----   Du bist tot!")
     print(" ¦                 /I\\") 
     print(" ¦                  I")
     print("---                / \\")
